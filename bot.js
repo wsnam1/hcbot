504ed4e193990f56bfa3a32afe3cc9b7e37cde7b
Woosung Nam <nws400@gmail.com>
2024-09-22 18:09:49 -0500

    port added to the bto

--- bot.js
+++ bot.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Discord bot is running!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
 const {
     Client,
     GatewayIntentBits,
